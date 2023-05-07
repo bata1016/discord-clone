b1870b5286bd9a6a6c541ff1873a16a325eac455
kawabata.hiroaki-at-786457076534 <blp0087bata@gmail.com>
2023-05-07 20:04:18 +0900

    チャットの入力欄を追加

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import './Chat.scss'
 import ChatHeader from './ChatHeader'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import GifIcon from '@mui/icons-material/Gif';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
 function Chat() {
   return (
     <div className='chat'>
       <ChatHeader />
       <div className='chat-message'></div>
-      <div className='chat-input'></div>
+      <div className='chat-input'>
+        <AddCircleOutlineIcon />
+        <form>
+          <input type="text" placeholder='#Udemyへメッセージを送信'/>
+          <button type='submit' className='chat-input-button'>送信</button>
+        </form>
+        <div className='chat-input-icons'>
+          <CardGiftcardIcon />
+          <GifIcon />
+          <EmojiEmotionsIcon />
+        </div>
+      </div>
     </div>
   )
 }
